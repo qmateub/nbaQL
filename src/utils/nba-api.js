2aa8ca84603f5fc9b0f89510c97134ff073cd786
qmateub <qmateub@yahoo.es>
2018-04-01 00:18:59 +0200

    filter player by having teams

--- src/utils/nba-api.js
+++ src/utils/nba-api.js
@@ -4,13 +4,15 @@ export const fetchPlayers = () => {
   return axios
     .get('http://data.nba.net/10s/prod/v1/2017/players.json')
     .then(response =>
-      response.data.league.standard.map(player => ({
-        playerId: player.personId,
-        image: `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${
-          player.personId
-        }.png`,
-        ...player,
-      }))
+      response.data.league.standard
+        .filter(player => player.teams.length > 0)
+        .map(player => ({
+          playerId: player.personId,
+          image: `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${
+            player.personId
+          }.png`,
+          ...player,
+        }))
     );
 };
 
